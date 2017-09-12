f3ffd39f571d75885f541a49912cfea115849cd0
g42gregory <g42gregory@gmail.com>
2017-09-12 15:04:43 -0700

    incremental

--- chapter03/grid-world.cpp
+++ chapter03/grid-world.cpp
@@ -4,6 +4,16 @@
 
 #include <iostream>
 
+using namespace std;
+
+int WORLD_SIZE = 5;
+pair<int, int> A_POS       {0, 1};
+pair<int, int> A_PRIME_POS {4, 1};
+pair<int, int> B_POS       {0, 3};
+pair<int, int> B_PRIME_POS {2, 3};
+double discount = 0.9;
+
+
 int main() {
     std::cout << "Hello, World!!!!" << std::endl;
     return 0;
